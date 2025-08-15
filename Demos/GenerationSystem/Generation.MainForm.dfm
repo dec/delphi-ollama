356d3a07f5aae69aaf2b70a8b2065c864b19765b
David Esperalta <davidesperalta@gmail.com>
2025-08-15 21:59:00 +0200

    Add GenerationSystem demo
    Add GenerationSystem demo

---
+++ Demos/GenerationSystem/Generation.MainForm.dfm
@@ -0,0 +1,129 @@
+object MainForm: TMainForm
+  Left = 0
+  Top = 0
+  Caption = 'DecSoft Ollama For Delphi Generation System Demo'
+  ClientHeight = 634
+  ClientWidth = 1004
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCloseQuery = FormCloseQuery
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  PixelsPerInch = 96
+  TextHeight = 13
+  object BottomPanel: TPanel
+    Left = 0
+    Top = 478
+    Width = 1004
+    Height = 156
+    Align = alBottom
+    TabOrder = 0
+    DesignSize = (
+      1004
+      156)
+    object PromptLabel: TLabel
+      Left = 16
+      Top = 16
+      Width = 34
+      Height = 13
+      Caption = 'Prompt'
+    end
+    object ModelLabel: TLabel
+      Left = 670
+      Top = 16
+      Width = 28
+      Height = 13
+      Anchors = [akTop, akRight]
+      Caption = 'Model'
+      ExplicitLeft = 632
+    end
+    object PromptMemo: TMemo
+      Left = 16
+      Top = 35
+      Width = 631
+      Height = 81
+      Anchors = [akLeft, akTop, akRight]
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -15
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      Lines.Strings = (
+        'What color is the sky?')
+      ParentFont = False
+      ScrollBars = ssVertical
+      TabOrder = 0
+    end
+    object ModelEdit: TEdit
+      Left = 670
+      Top = 35
+      Width = 177
+      Height = 21
+      Anchors = [akTop, akRight]
+      TabOrder = 1
+      Text = 'gemma3'
+    end
+    object GenerateButton: TButton
+      Left = 670
+      Top = 72
+      Width = 75
+      Height = 34
+      Anchors = [akTop, akRight]
+      Caption = 'Generate'
+      TabOrder = 2
+      OnClick = GenerateButtonClick
+    end
+    object CancelButton: TButton
+      Left = 772
+      Top = 72
+      Width = 75
+      Height = 34
+      Anchors = [akTop, akRight]
+      Caption = 'Cancel'
+      Enabled = False
+      TabOrder = 3
+      OnClick = CancelButtonClick
+    end
+    object StreamedCheckBox: TCheckBox
+      Left = 870
+      Top = 32
+      Width = 115
+      Height = 17
+      Anchors = [akTop, akRight]
+      Caption = 'Streamed response'
+      Checked = True
+      State = cbChecked
+      TabOrder = 4
+    end
+    object StatusBar: TStatusBar
+      Left = 1
+      Top = 136
+      Width = 1002
+      Height = 19
+      Panels = <>
+    end
+  end
+  object ResponseMemo: TMemo
+    Left = 0
+    Top = 0
+    Width = 1004
+    Height = 478
+    Align = alClient
+    DoubleBuffered = True
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -16
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentDoubleBuffered = False
+    ParentFont = False
+    ReadOnly = True
+    ScrollBars = ssVertical
+    TabOrder = 1
+  end
+end
---
+++ Demos/GenerationSystem/Generation.MainForm.pas
@@ -0,0 +1,128 @@
+unit Generation.MainForm;
+
+interface
+
+uses
+  Vcl.Forms,
+  Vcl.StdCtrls,
+  System.Classes,
+  Vcl.Controls,
+  Vcl.ExtCtrls,
+  Vcl.ComCtrls,
+
+  DecSoft.Ollama.Generation.Request;
+
+type
+  TMainForm = class(TForm)
+    ModelEdit: TEdit;
+    PromptMemo: TMemo;
+    ModelLabel: TLabel;
+    BottomPanel: TPanel;
+    ResponseMemo: TMemo;
+    PromptLabel: TLabel;
+    CancelButton: TButton;
+    StatusBar: TStatusBar;
+    GenerateButton: TButton;
+    StreamedCheckBox: TCheckBox;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure CancelButtonClick(Sender: TObject);
+    procedure GenerateButtonClick(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+  private
+    FRequest: TGenerationRequest;
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+uses
+  Vcl.Dialogs,
+  System.SysUtils,
+
+  DecSoft.Ollama.Generation.Types;
+
+{$R *.dfm}
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  FRequest := TGenerationRequest.Create();
+end;
+
+procedure TMainForm.CancelButtonClick(Sender: TObject);
+begin
+  FRequest.Stop();
+end;
+
+procedure TMainForm.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+begin
+  if FRequest.IsRunning then
+    FRequest.Stop();
+end;
+
+procedure TMainForm.FormDestroy(Sender: TObject);
+begin
+  FRequest.Free();
+end;
+
+procedure TMainForm.GenerateButtonClick(Sender: TObject);
+begin
+  ResponseMemo.Clear();
+
+  CancelButton.Enabled := True;
+  GenerateButton.Enabled := False;
+  Screen.Cursor := crHourGlass;
+
+  try
+    try
+
+      FRequest.Run(
+
+        procedure (var Params: TGenerationParams)
+        begin
+          Params.Model := ModelEdit.Text;
+          Params.Prompt := PromptMemo.Text;
+          Params.Stream := StreamedCheckBox.Checked;
+          Params.System := 'You are a fantastic magician who wants to amaze whoever listens to you.';
+        end,
+
+        procedure (const Result: TGenerationResponseResult; var Stop: Boolean)
+        begin
+          Application.ProcessMessages();
+
+          if Result.Streamed and not Result.Done then
+            ResponseMemo.Text := ResponseMemo.Text +
+             StringReplace(Result.Response, #10, #13#10, [rfReplaceAll]);
+
+          if not Result.Streamed and Result.Done then
+            ResponseMemo.Text := ResponseMemo.Text +
+             StringReplace(Result.Response, #10, #13#10, [rfReplaceAll]);
+        end,
+
+        procedure (const Error: string)
+        begin
+          ShowMessage(Format('Error: %s', [Error]));
+        end);
+
+    except
+
+      on E: Exception do
+      begin
+        ShowMessage(Format('Exception: %s', [E.Message]));
+      end;
+    end;
+
+  finally
+
+    Screen.Cursor := crDefault;
+    GenerateButton.Enabled := True;
+    CancelButton.Enabled := False;
+  end;
+end;
+
+initialization
+  ReportMemoryLeaksOnShutdown := True;
+
+end.
